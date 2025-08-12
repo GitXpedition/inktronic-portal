1d6773ccf78ead77a2aabf1e99211d73a736cf5c
GitXpedition <feldman.carlos@gmail.com>
2025-08-12 14:51:12 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -34,17 +34,23 @@ export default function Home() {
           <TranslatedText translationKey="page_status" />&nbsp;
         </p>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <div className="flex place-items-center gap-2 p-8 lg:p-0">
-            <TranslatedText translationKey="attribution_by" />{' '}
-            <Image
-              src="/catalogic_isologo.png"
-              alt="Logo inferior"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </div>
+
+{/*
+<div className="flex place-items-center gap-2 p-8 lg:p-0">
+  <TranslatedText translationKey="attribution_by" />{' '}
+  <Image
+    src="/catalogic_isologo.png"
+    alt="Logo inferior"
+    className="dark:invert"
+    width={100}
+    height={24}
+    priority
+  />
+</div>
+*/}
+
+
+          
         </div>
       </div>
 
